479d9a4d6a62dc0538dec4363995078d7aac5bd6
deepliya <50700950+deepliya@users.noreply.github.com>
2022-05-16 11:45:13 +0300

    Created helper function to load data of  csv file

--- insurance-costs-analysis.py
+++ insurance-costs-analysis.py
@@ -1,5 +1,7 @@
+# importing csv library
 import csv
 
+# Initializing lists for diffrent attributes in insurance.csv
 ages = []
 sexes = []
 bmis = []
@@ -7,3 +9,13 @@ num_children = []
 smoker_statuses = []
 regions = []
 insurance_charges = []
+
+# helper function to load csv data
+def load_list_data(lst, csv_file, column_name):
+    with open(csv_file) as csv_info:
+        csv_dict = csv.DictReader(csv_info) 
+        for row in csv_dict:
+            lst.append(row[column_name])
+
+load_list_data(ages, 'insurance.csv', 'age')
+print(ages)
