c0e8270cf6f51c7b48a3ab403bb55e3c1c0a69ad
deepliya <50700950+deepliya@users.noreply.github.com>
2022-05-16 14:10:30 +0300

    Created PatientsInfo class

--- insurance-costs-analysis.py
+++ insurance-costs-analysis.py
@@ -24,3 +24,15 @@ load_list_data(num_children, 'insurance.csv', 'children')
 load_list_data(smoker_statuses, 'insurance.csv', 'smoker')
 load_list_data(regions, 'insurance.csv', 'region')
 load_list_data(insurance_charges, 'insurance.csv', 'charges')
+
+class PatientsInfo:
+    # init method that takes in each list parameter
+    def __init__(self, patients_ages, patients_sexes, patients_bmis, patients_num_children, 
+                 patients_smoker_statuses, patients_regions, patients_charges):
+        self.patients_ages = patients_ages
+        self.patients_sexes = patients_sexes
+        self.patients_bmis = patients_bmis
+        self.patients_num_children = patients_num_children
+        self.patients_smoker_statuses = patients_smoker_statuses
+        self.patients_regions = patients_regions
+        self.patients_charges = patients_charges
