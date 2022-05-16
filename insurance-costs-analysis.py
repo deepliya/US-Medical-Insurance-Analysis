972737ccbc9595515e90c8872985bfdadc1ccb0b
deepliya <50700950+deepliya@users.noreply.github.com>
2022-05-16 14:37:38 +0300

    Created functions to inpect ages and sexes

--- insurance-costs-analysis.py
+++ insurance-costs-analysis.py
@@ -36,3 +36,26 @@ class PatientsInfo:
         self.patients_smoker_statuses = patients_smoker_statuses
         self.patients_regions = patients_regions
         self.patients_charges = patients_charges
+
+    # function that calculates the average ages of patients in insurance.csv
+    def analyze_ages(self):
+        total_age = 0
+        for age in self.patients_ages:
+            total_age += int(age)
+        print ("Average Patient Age: " + str(round(total_age/len(self.patients_ages), 2)) + " years")
+
+    # function that calculates the number of males and females in insurance.csv
+    def analyze_sexes(self):
+        females = 0
+        males = 0
+        for sex in self.patients_sexes:
+            if sex == 'female':
+                females += 1
+            elif sex == 'male':
+                males += 1
+        print("Count for female: ", females)
+        print("Count for male: ", males)
+
+patient_info = PatientsInfo(ages, sexes, bmis, num_children, smoker_statuses, regions, insurance_charges)
+patient_info.analyze_ages()
+patient_info.analyze_sexes()
