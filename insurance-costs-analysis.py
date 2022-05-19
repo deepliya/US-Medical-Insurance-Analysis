2780728ae6eb4335761fb74c4cd66a21956d4bd6
deepliya <50700950+deepliya@users.noreply.github.com>
2022-05-19 16:11:47 +0300

    Added comments

--- insurance-costs-analysis.py
+++ insurance-costs-analysis.py
@@ -1,8 +1,8 @@
-# importing csv library
+# import csv and statistics libraries
 import csv
 import statistics
 
-# Initializing lists for diffrent attributes in insurance.csv
+# Initialize lists for diffrent attributes in insurance.csv
 ages = []
 sexes = []
 bmis = []
@@ -14,10 +14,14 @@ insurance_charges = []
 # helper function to load csv data
 def load_list_data(lst, csv_file, column_name):
     with open(csv_file) as csv_info:
-        csv_dict = csv.DictReader(csv_info) 
+        # open csv file
+        csv_dict = csv.DictReader(csv_info)
+        # loop through the data in each row of the csv  
         for row in csv_dict:
+            # add the data from each row to a list
             lst.append(row[column_name])
 
+# Load data from the csv file into respective lists
 load_list_data(ages, 'insurance.csv', 'age')
 load_list_data(sexes, 'insurance.csv', 'sex')
 load_list_data(bmis, 'insurance.csv', 'bmi')
@@ -26,6 +30,7 @@ load_list_data(smoker_statuses, 'insurance.csv', 'smoker')
 load_list_data(regions, 'insurance.csv', 'region')
 load_list_data(insurance_charges, 'insurance.csv', 'charges')
 
+# Convert list elements into appropriate types to aid analysis
 insurance_charges = [float(charge) for charge in insurance_charges ]
 num_children = [int(child) for child in num_children] 
 ages = [int(age) for age in ages] 
@@ -43,8 +48,9 @@ class PatientsInfo:
         self.patients_regions = patients_regions
         self.patients_charges = patients_charges
 
-    # function that calculates the average of charges for patients in different age groups
+    # function that calculates the average charges for patients in different age groups
     def analyze_ages(self):
+        #intitializing total_age and counters for all age classes and their respective charges
         total_age = 0
         age_20 = 0
         age_30 = 0
@@ -60,6 +66,7 @@ class PatientsInfo:
         for age in self.patients_ages:
             total_age += age
 
+        # Looping through the dataset to find out how many people are in each age group and their total charges for each
         for age, charge in zip(self.patients_ages, self.patients_charges):
             if age < 20:
                 age_20 += 1
@@ -89,12 +96,15 @@ class PatientsInfo:
 
     # function that calculates the number of males and females and the average charges for each group in insurance.csv
     def analyze_sexes(self):
+        # Intialize counters for number of males and females and their respective cumulative charges
         females = 0
         males = 0
         female_charges = 0
         male_charges = 0
         avg_female = 0
         avg_male = 0
+
+        # loop through sex and charge columns to count number of males and females and their respective charges
         for sex, charge in zip(self.patients_sexes, self.patients_charges):
             if sex == 'female':
                 females += 1
@@ -112,6 +122,7 @@ class PatientsInfo:
         print("    Average charge for a male: $", avg_male, "\n")
     
     def analyze_bmi(self):
+        # Intialize counters for each BMI class and their respective charge counters
         undr_wt = 0
         norm_wt = 0
         ovr_wt = 0
@@ -121,6 +132,7 @@ class PatientsInfo:
         ovr_ch = 0
         obese_ch = 0
 
+        # loop through bmi and charge columns to count number of people in different BMI classes and their respective charges
         for bmi, charge in zip(self.patients_bmis, self.patients_charges):
             if bmi < 18.5:
                 undr_wt += 1
@@ -181,6 +193,7 @@ class PatientsInfo:
 
     # method to find average yearly medical charges for patients in insurance.csv
     def charges_stats(self):
+        #Calculating mean, variance and standard deviation of total charges of all people in the dataset
         mean = round(statistics.mean(insurance_charges), 2)
         variance = round(statistics.pvariance(insurance_charges), 2)
         st_dev = round(statistics.pstdev(insurance_charges), 2)
@@ -206,7 +219,10 @@ class PatientsInfo:
         self.patients_dictionary["charges"] = self.patients_charges
         return self.patients_dictionary
 
+# creating an instance of PatientsInfo class to call different function on the dataset
 patient_info = PatientsInfo(ages, sexes, bmis, num_children, smoker_statuses, regions, insurance_charges)
+
+# calling all functions to analyze the dataset with respect to different attributes
 patient_info.analyze_ages()
 patient_info.analyze_sexes()
 patient_info.analyze_bmi()
@@ -214,4 +230,3 @@ patient_info.analyze_smokers()
 patient_info.unique_regions()
 patient_info.charges_stats()
 PatDict=patient_info.create_dictionary()
-#print(PatDict["charges"])
